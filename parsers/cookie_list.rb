f7be44b1a2a7bb6bb300d40689b1dae2b477b222
ayarzuki <aulya.yarzuki@gmail.com>
2022-03-15 00:16:03 +0700

    added listings file and product files with script

---
+++ parsers/cookie_list.rb
@@ -0,0 +1,69 @@
+search_terms = ['Red Bull', 'RedBull', 'Energy Drink', 'Energidrikk']
+
+if page['vars']['page_type'] == "search"
+    search_terms.each do |search_term|
+        if page['vars']['search_term'] == search_term
+            csrf_token = page['response_cookie'].split("\; ")[0].split("=").last
+            pages << {
+                page_type: 'listings',
+                no_url_encode: true,
+                url: "https://platform-rest-prod.ngdata.no/api/episearch/1210/products?search=#{search_term.gsub(" ","%20")}&page_size=20&suggest=true&page=1&types=suggest,products,articles,recipes,stores&store_id=7080001097950&popularity=true&sort=&facet=;;;;;;&full_response=true&showNotForSale=true",
+                headers: {
+                    'Host' => 'platform-rest-prod.ngdata.no',
+                    'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0',
+                    'Accept' => '*/*',
+                    'Referer' => "https://spar.no/sok?query=#{search_term.gsub(' ', '+').downcase}&expanded=products",
+                    'content-type' => 'application/json',
+                    'fwc-browser-name' => 'Firefox',
+                    'fwc-browser-platform' => 'Linux x86_64',
+                    'fwc-browser-version' => '98.0',
+                    'fwc-chain-id' => '1210',
+                    'fwc-framework-versions' => '"unknown"',
+                    'fwc-using-api-key' => 'false',
+                    'fwc-using-bearer-token' => 'false',
+                    'fwc-using-csrf-token' => 'true',
+                    'x-csrf-token' => csrf_token,
+                },
+                # http2: true,
+                vars: {
+                    page_number: 1,
+                    page_type: 'search',
+                    page_category: '-',
+                    search_term: search_term
+                }
+            }
+        end
+    end
+else
+    csrf_token = page['response_cookie'].split("\; ")[0].split("=").last
+    pages << {
+        page_type: 'listings',
+        no_url_encode: true,
+        url: "https://platform-rest-prod.ngdata.no/api/products/1210/7080001097950?page=1&page_size=40&full_response=true&fieldset=maximal&facets=Category,Allergen&facet=Categories:Drikke;ShoppingListGroups:Energidrikk&showNotForSale=true",
+        headers: {
+            'Host' => 'platform-rest-prod.ngdata.no',
+            'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0',
+            'Accept' => '*/*',
+            'Referer' => "https://spar.no/nettbutikk/varer/drikke/energidrikk",
+            'content-type' => 'application/json',
+            'fwc-browser-name' => 'Firefox',
+            'fwc-browser-platform' => 'Linux x86_64',
+            'fwc-browser-version' => '98.0',
+            'fwc-chain-id' => '1210',
+            'fwc-framework-versions' => '"unknown"',
+            'fwc-using-api-key' => 'false',
+            'fwc-using-bearer-token' => 'false',
+            'fwc-using-csrf-token' => 'true',
+            'x-csrf-token' => csrf_token,
+        },
+        # http2: true,
+        vars: {
+            page_number: 1,
+            page_type: 'taxonomy',
+            page_category: 'Energidrikk',
+            search_term: '-'
+        }
+    }
+end
+
+# puts page['vars']['page_type'] == "listings"
---
+++ parsers/product.rb
@@ -0,0 +1,93 @@
+vars = page['vars']
+
+parser_page = Nokogiri.HTML(content)
+
+#title = parser_page.at('h1').text.gsub(/(\n\r|\n|\r|\t|\s|<.*>)+/, ' ').strip
+#title = "#{title} #{parser_page.at('.cw-product__variant').text.gsub(/(\n\r|\n|\r|\t|\s)+/, ' ').strip}" rescue title
+id = page['url'].scan(/(\d+)\?/).first.first
+promotion_text = parser_page.at('.cw-product__over .cw-product__promotion').text.strip rescue ''
+brand = parser_page.at('h1').text.gsub(/(\n\r|\n|\r|\t|\s)+/, ' ').strip 
+brand = 'Red Bull' if brand.include?('Red Bull')
+
+[
+  /(\d*[\.,]?\d+)\s?([Ff][Ll]\.?\s?[Oo][Zz])/,
+  /(\d*[\.,]?\d+)\s?([Oo][Zz])/,
+  /(\d*[\.,]?\d+)\s?([Ff][Oo])/,
+  /(\d*[\.,]?\d+)\s?([Ee][Aa])/,
+  /(\d*[\.,]?\d+)\s?([Ff][Zz])/,
+  /(\d*[\.,]?\d+)\s?(Fluid Ounces?)/,
+  /(\d*[\.,]?\d+)\s?([Oo]unce)/,
+  /(\d*[\.,]?\d+)\s?([Mm]l)/,
+  /(\d*[\.,]?\d+)\s?([Ll])/,
+  /(\d*[\.,]?\d+)\s?(cc)/i,
+  /(\d*[\.,]?\d+)\s?([Cc][Ll])/,
+  /(\d*[\.,]?\d+)\s?([Gg])/,
+  /(\d*[\.,]?\d+)\s?([Gg]r)/i,
+  /(\d*[\.,]?\d+)\s?([Ll]itre)/,
+  /(\d*[\.,]?\d+)\s?([Ll]itro[s]?)/i,
+  /(\d*[\.,]?\d+)\s?([Ll]ts)/i,
+  /(\d*[\.,]?\d+)\s?([Ss]ervings)/,
+  /(\d*[\.,]?\d+)\s?([Pp]acket\(?s?\)?)/,
+  /(\d*[\.,]?\d+)\s?([Cc]apsules)/,
+  /(\d*[\.,]?\d+)\s?([Tt]ablets)/,
+  /(\d*[\.,]?\d+)\s?([Tt]ubes)/,
+  /(\d*[\.,]?\d+)\s?([Cc]hews)/
+].find { |regexp| title =~ regexp }
+item_size = $1
+uom = $2
+
+match = [
+  /x\s(\d+)$/i,
+  /x\s?(\d+)\sboks$/i,
+  /(\d+)\s?und/i,
+  /Pack of (\d+)/,
+  /(\d+)\s?Pack/i,
+  /Pack (\d+)/i,
+  /Box of (\d+)/,
+  /Case of (\d+)/,
+  /(\d+)\s?[Cc]ount/,
+  /(\d+)\s?unidades/i,
+  /(\d+)\s?[Cc][Tt]/,
+  /(\d+)[\s-]?Pack($|[^e])/,
+  /(\d+)\s?[Pp][Kk]/
+].find { |regexp| title =~ regexp }
+in_pack = match ? $1 : '1'
+in_pack = '4' if title.include?('Four Pack')
+in_pack = '2' if title.include?('Duo Pack')
+
+description = parser_page.at('.cw-product__description').text.gsub(/\.\.\.|Les mer/, '').strip rescue nil
+image_url = parser_page.at('.cw-product__image img').attr('src')
+price = parser_page.at('span[itemprop="price"]').text
+is_available = parser_page.at('.cw-product__unavailable').nil? ? 1 : ''
+
+info = {
+  _collection: 'products',
+  RETAILER_ID: '0191',
+  RETAILER_NAME: 'spar_no',
+  GEOGRAPHY_NAME: 'NO',
+  # - - - - - - - - - - -
+  SCRAPE_INPUT_TYPE: vars['page_type'],
+  SCRAPE_INPUT_SEARCH_TERM: vars['search_term'],
+  SCRAPE_INPUT_CATEGORY: vars['page_category'],
+  SCRAPE_URL_NBR_PRODUCTS: vars['total'],
+  SCRAPE_URL_NBR_PROD_PG1: vars['total_pg1'],
+  # - - - - - - - - - - -
+  PRODUCT_BRAND: brand,
+  PRODUCT_RANK: vars['rank'],
+  PRODUCT_PAGE: vars['page_number'],
+  PRODUCT_ID: id,
+  PRODUCT_NAME: title,
+  PRODUCT_DESCRIPTION: description,
+  PRODUCT_MAIN_IMAGE_URL: image_url,
+  PRODUCT_ITEM_SIZE: (item_size rescue ''),
+  PRODUCT_ITEM_SIZE_UOM: (uom rescue ''),
+  PRODUCT_ITEM_QTY_IN_PACK: (in_pack rescue ''),
+  PRODUCT_STAR_RATING: '',
+  PRODUCT_NBR_OF_REVIEWS: '',
+  SALES_PRICE: price,
+  IS_AVAILABLE: is_available,
+  PROMOTION_TEXT: promotion_text,
+  EXTRACTED_ON: Time.now.to_s
+}
+
+outputs << info
